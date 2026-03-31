e22551d7c0cb2bfc5407262b7ab6fac239f7b9e2
Emily Moy <emoynj@gmail.com>
2026-03-31 17:59:53 -0400

    adding comment to pre processing code

--- generating_csv.py
+++ generating_csv.py
@@ -14,6 +14,9 @@ df_only_regina_strokes = df[df['artists'].isin(['Regina Spektor', 'The Strokes']
 # DataFrame with NO Regina or The Strokes
 df_no_regina_strokes = df[~df['artists'].isin(['Regina Spektor', 'The Strokes'])]
 
+# We knew there were 55 songs by Regina and the Strokes combined since we filtered
+# the original CSV file for those artists when we first downloaded it.
+ 
 # randomly choosing 1945 songs from the DataFrame with no Regina or The Strokes
 base_random_2000 = df_no_regina_strokes.sample(n=1945, random_state=42)
 # combining the 1945 random songs with the 55 Regina or Strokes songs
